a6933c51de3abc8c81442474766f45519abe8809
gokul278 <gokulhk78gmail.com>
2024-09-18 17:43:13 +0530

    Responsive Design

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <div className="w-[100%]" align="center">
         <div className="w-[90%] flex justify-center gap-x-10" align="center">
-          <div className="w-[50%] bg-[#F5F7F8]">
+          <div className="w-[100%] lg:w-[50%] bg-[#F5F7F8]">
             <TailwindCss />
           </div>
 
